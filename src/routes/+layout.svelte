d8fad849894fcf41fef281cb285604a4f260b159
Matthew deHaas <mattdehaas28@gmail.com>
2026-04-04 00:58:35 -0700

    Add fonts.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+  import '@fontsource/playfair-display/400.css';
+  import '@fontsource/playfair-display/700.css';
+  import '@fontsource/cormorant-garamond/400.css';
+  import '@fontsource/cormorant-garamond/600.css';
 
 	let { children } = $props();
 </script>
