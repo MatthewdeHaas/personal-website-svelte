ced2e8c1b73a27d76813d7f801647b0af1c5ccb1
Matthew deHaas <mattdehaas28@gmail.com>
2026-04-07 11:48:49 -0700

    Fix postgres losing connection.

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,11 @@
 import postgres from "postgres";
 import { env } from "$env/dynamic/private";
 
-const sql = postgres(env.DATABASE_URL);
+const sql = postgres(env.DATABASE_URL, {
+  max: 10,
+  idle_timeout: 20,
+  connect_timeout: 10,
+  max_lifetime: 60 * 30,
+});
 
 export { sql };
